f83c788ff2e5f58566acef4538dc25fa8283202c
chethan <mchethan621@gmail.com>
2026-09-13 09:59:25 +0530

    updates flight  test cases

--- src/test/java/tests/flightTest.java
+++ src/test/java/tests/flightTest.java
@@ -1,17 +1,26 @@
 package tests;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.microsoft.playwright.FrameLocator;
+import com.microsoft.playwright.Page;
 
 import base.BaseTest;
 import pages.flightPage;
 
-public class flightTest extends BaseTest {
 
+public class flightTest extends BaseTest {
+	
+	private flightPage flightPage;
+	
+    @BeforeClass
+    public void createPageObject() {
+        flightPage = new flightPage(page, context);
+    }
+    
 	@Test
 	public void flightSearchTest() {
-
-	    flightPage flightPage = new flightPage(page, context);
-	    
 	    flightPage.selectOneway();
 	    flightPage.selectRoundTrip();
 	    flightPage.selectOrigin("Bengaluru");
@@ -22,5 +31,27 @@ public class flightTest extends BaseTest {
 	    page.waitForTimeout(2000);
 	    flightPage.searchFlights();	  
 	}
+	
+	@Test
+	public void popUpTest() {
+		//alert and pop up for practice
+		page.waitForTimeout(2000);
+		page.navigate("https://rahulshettyacademy.com/AutomationPractice/");
+		//page.onDialog(pop -> pop.accept());
+		page.onceDialog(pop -> pop.accept());
+		page.getByText("Alert", new Page.GetByTextOptions().setExact(true)).click();
+		page.onceDialog(pop -> pop.dismiss());
+		page.getByText("Confirm", new Page.GetByTextOptions().setExact(true)).click();
+	}
+	
+	@Test
+	public void framesTest() {
+		//frames 
+		page.navigate("https://demo.automationtesting.in/Frames.html");
+		FrameLocator framepage= page.frameLocator("#singleframe");
+		framepage.locator("input[type='text']").fill("I am Chethan M a software engineer in testing");
+		page.waitForTimeout(2000);
+	}
+	
 
 }
